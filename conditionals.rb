7c56b4f5e2b1d2a20bb1f8dca4313d5fe670489d
Miray Karabulut <mimikarabulut@gmail.com>
2023-03-22 14:34:27 -0500

    less than ten and greater than 10

--- conditionals.rb
+++ conditionals.rb
@@ -13,7 +13,9 @@ end
 
 number = 12.5 
 if number < 10 
-  p 1 
+  p -1 
+elsif number <10 
+  p 1
 end 
 
 # 3. Use variables to store two numbers, then write a condition that prints 1 if the numbers are both less than 10, and prints 0 otherwise.
