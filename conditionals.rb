f10d1e9068492e7bcb2cb4ecd841e6c4bb743ded
Miray Karabulut <mimikarabulut@gmail.com>
2023-03-22 18:08:35 -0500

    elsif number > 20 p 2

--- conditionals.rb
+++ conditionals.rb
@@ -102,7 +102,7 @@
 
 # 10. Use a variable to store a number, then write a condition that prints 5 if the number is greater than 80, prints 4 if the number is greater than 60, prints 3 if the number is greater than 40, prints 2 if the number is greater than 20, and prints 1 otherwise (only one print statement should occur).
 
-number = 21
+number = 1
 
 if number > 80 
   p 5
@@ -112,6 +112,8 @@ elsif number > 40
   p 3
 elsif number > 20 
   p 2 
+else 
+  p 1 
 end 
 
 
