d7e493327cdabb71dea6a58c3f4185b6ac16bdb9
Miray Karabulut <mimikarabulut@gmail.com>
2023-03-22 14:40:07 -0500

    try code with number less than 10, doesn't work

--- conditionals.rb
+++ conditionals.rb
@@ -11,10 +11,11 @@ end
 
 # 2. Use a variable to store a number, then write a condition that prints -1 if the number is less than 10, prints 1 if the number is greater than 10, and prints 0 if the number is equal to 10.
 
-number = 12 
+number = 9 
+
 if number < 10
   p -1 
-elsif number < 10
+elsif number > 10
   p 1
 elsif  
   number == 10
