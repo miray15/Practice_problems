3fc315a502148bf4122124e304739b668f9dfbc0
Miray Karabulut <mimikarabulut@gmail.com>
2023-03-22 18:13:57 -0500

    write first name and last name in string form

--- conditionals.rb
+++ conditionals.rb
@@ -102,19 +102,19 @@
 
 # 10. Use a variable to store a number, then write a condition that prints 5 if the number is greater than 80, prints 4 if the number is greater than 60, prints 3 if the number is greater than 40, prints 2 if the number is greater than 20, and prints 1 otherwise (only one print statement should occur).
 
-number = 1
+# number = 1
 
-if number > 80 
-  p 5
-elsif number > 60 
-  p 4 
-elsif number > 40 
-  p 3
-elsif number > 20 
-  p 2 
-else 
-  p 1 
-end 
+# if number > 80 
+#   p 5
+# elsif number > 60 
+#   p 4 
+# elsif number > 40 
+#   p 3
+# elsif number > 20 
+#   p 2 
+# else 
+#   p 1 
+# end 
 
 
 # SOLUTIONS: https://gist.github.com/peterxjang/aee70f966f0f725609eee89b06e8a6c0
