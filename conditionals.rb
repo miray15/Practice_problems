5e63745b048adf4add0d49e1fddb99267dfbb592
Miray Karabulut <mimikarabulut@gmail.com>
2023-03-22 17:56:31 -0500

    else print 0 works

--- conditionals.rb
+++ conditionals.rb
@@ -78,12 +78,14 @@
 
 # 8. Use a variable to store a number, then write a condition that prints 100 if the number equals 100, prints 99 if the number is equal to 99, and prints 0 otherwise.
 
-number = 99 
+number = 101 
 
 if  number == 100 
   p 100 
 elsif number == 99 
   p 99
+else 
+  p 0 
 end 
 
 # 9. Use variables to store two numbers, then write a condition that prints 1 if the first number is less than zero and the second number is greater than 0, and prints 0 otherwise.
