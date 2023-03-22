e8d0b3ea2c4d9a351d830dd5b4057201bbd63f63
Miray Karabulut <mimikarabulut@gmail.com>
2023-03-22 14:32:55 -0500

    prints numbers more, less, and equal to 10

--- conditionals.rb
+++ conditionals.rb
@@ -11,6 +11,11 @@ end
 
 # 2. Use a variable to store a number, then write a condition that prints -1 if the number is less than 10, prints 1 if the number is greater than 10, and prints 0 if the number is equal to 10.
 
+number = 12.5 
+if number < 10 
+  p 1 
+end 
+
 # 3. Use variables to store two numbers, then write a condition that prints 1 if the numbers are both less than 10, and prints 0 otherwise.
 
 # 4. Use a variable to store a number, then write a condition that prints 1 if the number is over 9000, and prints -1 otherwise.
