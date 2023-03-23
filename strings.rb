83d8d1d83adc2b361082e024ad0b2012def8e4aa
Miray Karabulut <mimikarabulut@gmail.com>
2023-03-23 11:21:33 -0500

    Add first name and last name

--- strings.rb
+++ strings.rb
@@ -1,12 +1,17 @@
 # 1. Write a program that uses variables to store a first and last name, then prints the full name in one line using string concatenation (the + operator).
 
-first_name = "Miray " 
-last_name = "Karabulut" 
-p first_name + last_name
+# first_name = "Miray " 
+# last_name = "Karabulut" 
+# p first_name + last_name
 
 
 
 # 2. Write a program that uses variables to store a first and last name, then prints the full name in one line using string interpolation (the #{} operator).
+
+first_name = Karol 
+last_name = Fallas 
+
+
 
 # 3. Write a program that asks the user to input a word. If the word is "marco", print "polo".
 
