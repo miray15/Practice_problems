d56172441db9fecf15ac4971ad7bb97798e3d441
Miray Karabulut <mimikarabulut@gmail.com>
2023-03-23 11:23:47 -0500

    Print first name and last name

--- strings.rb
+++ strings.rb
@@ -8,10 +8,11 @@
 
 # 2. Write a program that uses variables to store a first and last name, then prints the full name in one line using string interpolation (the #{} operator).
 
-first_name = Karol 
-last_name = Fallas 
+first_name = "Karol"
 
-
+last_name = "Fallas"
+ 
+p full_name = "This person's name is #{first_name} #{last_name}"
 
 # 3. Write a program that asks the user to input a word. If the word is "marco", print "polo".
 
