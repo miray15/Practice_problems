1fc31133559511a2ad89a35252e666505075945d
Miray Karabulut <mimikarabulut@gmail.com>
2023-03-27 10:15:19 -0500

    add two numbers with .to_i

--- methods.rb
+++ methods.rb
@@ -6,15 +6,22 @@
 
 # 2. Write a program that asks the user to enter a number, then prints "That's a big number" if the number is greater than 100.
 
-puts "Enter a number"
-number = gets.chomp
-if number.to_i >= 100 
-  p "That's a big number!"
-else 
-  p "That's a small number."
-end 
+# puts "Enter a number"
+# number = gets.chomp
+# if number.to_i >= 100 
+#   p "That's a big number!"
+# else 
+#   p "That's a small number."
+# end 
 
 # 3. Write a program that asks the user to enter two numbers, then prints the numbers added together.
+
+p "Enter a number"
+number_1 = gets.chomp 
+p "Enter another number"
+number_2 = gets.chomp 
+
+p number_1.to_i + number_2.to_i
 
 # 4. Write a program that asks the user to enter a word, then prints that word in reverse order.
 
