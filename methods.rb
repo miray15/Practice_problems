dc725af12c1eaeb1557399a4e479a5c3ff4992c2
Miray Karabulut <mimikarabulut@gmail.com>
2023-03-27 10:09:55 -0500

    convert to integer that's a big number

--- methods.rb
+++ methods.rb
@@ -1,10 +1,18 @@
 # 1. Write a program that asks the user to enter a word, then prints that word with all capital letters.
 
-p "Enter a word"
-word = gets.chomp.upcase
-p word 
+# p "Enter a word"
+# word = gets.chomp.upcase
+# p word 
 
 # 2. Write a program that asks the user to enter a number, then prints "That's a big number" if the number is greater than 100.
+
+puts "Enter a number"
+number = gets.chomp
+if number.to_i >= 100 
+  p "That's a big number!"
+else 
+  p "That's a small number."
+end 
 
 # 3. Write a program that asks the user to enter two numbers, then prints the numbers added together.
 
