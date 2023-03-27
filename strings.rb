f08a39f4e8a468887b6143d88dfe21bd2475b7ac
Miray Karabulut <mimikarabulut@gmail.com>
2023-03-27 09:32:24 -0500

    grocery store colors

--- strings.rb
+++ strings.rb
@@ -24,13 +24,19 @@
 
 # 4. Write a program that uses variables to store three different colors, then prints out a sentence using the colors with string concatenation (the + operator).
 
-color_1 = "navy"  
-color_2 = "magenta" 
-color_3 = "burgundy" 
+# color_1 = "navy"  
+# color_2 = "magenta" 
+# color_3 = "burgundy" 
 
-p "The three most underrepresented colors for walls painted in medical offices are " + color_1 + ", " + color_2 + ", and" + color_3 + "."
+# p "The three most underrepresented colors for walls painted in medical offices are " + color_1 + ", " + color_2 + ", and" + color_3 + "."
 
 # 5. Write a program that uses variables to store three different colors, then prints out a sentence using the colors with string interpolation (the #{} operator).
+
+color_1 = "pink"  
+color_2 = "teal" 
+color_3 = "raisin" 
+
+p "The three most underrepresented colors in grocery stores are #{color_1}, #{color_2}, and #{color_3}." 
 
 # 6. Write a program that asks the user to enter a name. If the name is not "Santa", print "You're not Santa."
 
