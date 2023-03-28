51b83e552d608ebda41c1f8b41d15881c8165d26
Miray Karabulut <mimikarabulut@gmail.com>
2023-03-28 17:48:33 -0500

    hello five times

--- loops.rb
+++ loops.rb
@@ -1,12 +1,19 @@
 # 1. Write a while loop to print the numbers 1 through 10.
 
-number = 0 
-while number < 11 
-  p number 
-  number += 1 
-end 
+# number = 0 
+# while number < 11 
+#   p number 
+#   number += 1 
+# end 
 
 # 2. Write a while loop that prints the word "hello" 5 times.
+
+count = 0 
+while 
+  count < 5 
+  p "hello" 
+  count += 1
+end 
 
 # 3. Write a while loop that asks the user to enter a word and will run forever until the user enters the word "stop".
 
