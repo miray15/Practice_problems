c161719cf1c7e19d26e49cc7b11c81e99c3b23ef
Miray Karabulut <mimikarabulut@gmail.com>
2023-03-28 17:53:05 -0500

    prints numbers 0 to 100 in increments of 5

--- loops.rb
+++ loops.rb
@@ -17,15 +17,21 @@
 
 # 3. Write a while loop that asks the user to enter a word and will run forever until the user enters the word "stop".
 
-while true 
-  p "Enter a word"
-  word = gets.chomp 
-  if word == "stop"
-    break 
-  end 
-end 
+# while true 
+#   p "Enter a word"
+#   word = gets.chomp 
+#   if word == "stop"
+#     break 
+#   end 
+# end 
 
 # 4. Write a while loop that prints the numbers 0 through 100, increasing by 5 each time.
+
+number = 0
+while number < 105 
+  p number 
+  number += 5
+end 
 
 # 5. Write a while loop that prints the number 9000 ten times.
 
