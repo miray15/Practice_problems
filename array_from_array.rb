c3d9d3d1f27826d54903ef9e04ce5a9bbd236811
Miray Karabulut <mimikarabulut@gmail.com>
2023-04-03 10:00:41 -0500

    initial commit - array_from_array

--- array_from_array.rb
+++ array_from_array.rb
@@ -1,72 +1,20 @@
 # 1. Create an array to store 3 words. Then add two more words to the array and print the array on one line.
 
-# words = ["hello", "there", "captain"]
-# p words 
-
 # 2. Create an array to store 4 letters. Then change the second letter to a number and print the array on one line.
-
-# letters = ["a", "c", "d", "c"]
-# letters [1] = 7
-# p letters 
-
 
 # 3. Create an array to store 5 numbers. Then print out each number on separate lines with a while loop.
 
-# numbers = [1, 2, 3, 5, 4,]
-# i = 0
-
-# while i < numbers.length 
-#   puts numbers[i] 
-#   i = i + 1 
-# end 
-
 # 4. Create an array to store 1 number. Then add three more numbers to the array and print the array on one line.
-
-# storing = [3]
-# storing << 1 
-# storing << 4
-# storing << 3 
-# p storing 
 
 # 5. Create an array to store 3 strings with lower case letters. Then change the third string to have all capital letters and print the array on one line.
 
-# lower_case = ["c", "a", "t"]
-# lower_case[2] = lower_case[2].upcase
-# p lower_case
-
 # 6. Create an array to store 3 names. Then print out each name on separate lines with a while loop.
 
-# names = ["Joanna", "Alfred", "Jamal"]
-# i = 0 
-# while i < names.length 
-#   p names[i]
-#   i = i + 1 
-# end 
-
-# # 7. Create an array to store 2 strings. Then add one string to the array and print the array on one line.
-
-# strings = ["Hello", "there"]
-# strings << "captain"
-# p strings 
+# 7. Create an array to store 2 strings. Then add one string to the array and print the array on one line.
 
 # 8. Create an array to store 5 numbers. Then change the first number to 10 times its original value and print the array on one line.
 
-# numbers = [10, 20, 30, 40, 50]
-# numbers[0] = numbers[0] * 10
-# p numbers 
-
 # 9. Create an array to store 2 numbers. Then print out each number on separate lines with a while loop.
 
-# array = [3, 10]
-
-# puts array 
-
 # 10. Create an array to store names of 3 different countries. Then add one more country and print the array one line.
-
-# countries = ["Australia", "New Zealand", "Georgia"]
-
-# countries << "Malaysia"
-
-# p countries
-
 
