44b9ec157c356d685a4cf6d8769bd5507b07853b
Miray Karabulut <mimikarabulut@gmail.com>
2023-04-03 10:34:31 -0500

    array stores 5 number make number[1] 10 times value

--- array_from_array.rb
+++ array_from_array.rb
@@ -51,11 +51,15 @@
 
 # 7. Create an array to store 2 strings. Then add one string to the array and print the array on one line.
 
-strings = ["hi", "there"]
-strings << "Paul"
-p strings   
+# strings = ["hi", "there"]
+# strings << "Paul"
+# p strings   
 
 # 8. Create an array to store 5 numbers. Then change the first number to 10 times its original value and print the array on one line.
+
+numbers = [10, 10, 10, 10, 10]
+numbers[0] = numbers[0] * 10 
+p numbers 
 
 # 9. Create an array to store 2 numbers. Then print out each number on separate lines with a while loop.
 
