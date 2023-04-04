83681c41877858b8bf4a08567b65052d2f55efcf
Miray Karabulut <mimikarabulut@gmail.com>
2023-04-04 09:56:30 -0500

    store fn ln email and print on separate lines

--- array_from_array.rb
+++ array_from_array.rb
@@ -72,6 +72,6 @@
 
 # 10. Create an array to store names of 3 different countries. Then add one more country and print the array one line.
 
-countries = ["Germany", "France", "Iceland"]
-countries << "Greenland"
-p countries 
+# countries = ["Germany", "France", "Iceland"]
+# countries << "Greenland"
+# p countries 
