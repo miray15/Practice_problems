1f19470bae819d691f8330454c0066cf1c71a8f3
Miray Karabulut <mimikarabulut@gmail.com>
2023-04-06 18:46:27 -0500

    functions number to string

--- functions.rb
+++ functions.rb
@@ -36,18 +36,31 @@
 
 # 5. Write a function that takes in a string and returns the first letter of the string. Then run the function and print the result.
 
-def first_let(string)
-  return string.chr
-end 
+# def first_let(string)
+#   return string.chr
+# end 
 
-p first_let("hello")
+# p first_let("hello")
 
 # name = "Jason"
 # p name.chr
 
 # 6. Write a function that takes in three strings and returns a string that combines all three strings with spaces in between. Then run the function and print the result.
 
+# def combines(string1, string2, string3)
+#   return "#{string1} #{string2} #{string3}" 
+# end 
+
+# p combines("the", "green", "apple")
+
 # 7. Write a function that takes in a number and returns the number as a string. Then run the function and print the result.
+
+def num_to_string(number)
+  return number = number.to_s 
+end 
+
+p num_to_string(555)
+
 
 # 8. Write a function that takes in a string and returns the string repeated 5 times. Then run the function and print the result.
 
