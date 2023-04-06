55c28e41c726a1e3b58294de7773dd8efe9cc6b3
Miray Karabulut <mimikarabulut@gmail.com>
2023-04-06 17:35:38 -0500

    hash write 3 laptops and print model of second one

--- hash.rb
+++ hash.rb
@@ -62,13 +62,22 @@
 
 # 7. Make a hash to store a laptop's brand, model, and year. Then print each attribute on separate lines.
 
-laptop = {"brand" => "Apple", "model" => "MacBook Air", "year" => "2022"}
+# laptop = {"brand" => "Apple", "model" => "MacBook Air", "year" => "2022"}
 
-p laptop["brand"]
-p laptop["model"]
-p laptop["year"]
+# p laptop["brand"]
+# p laptop["model"]
+# p laptop["year"]
 
 # 8. Make an array of hashes to store the brand and model for 3 different laptops. Then print out the second laptop's model.
+
+laptops = [
+{"brand" => "Dell", "model" => "XPS"}, 
+{"brand" => "HP", "model" => "thinkpad"}, 
+{"brand" => "Apple", "model" => "air"}
+
+]
+
+p laptops[1]["model"]
 
 # 9. Make a hash to store definitions for 2 different words. Then add a new word and definition and print the hash to see the result.
 
