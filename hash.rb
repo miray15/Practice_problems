3a288b5e38623fbfa9c7e1996f11394f7fc9632d
Miray Karabulut <mimikarabulut@gmail.com>
2023-04-06 17:41:25 -0500

    hash store 2 words add another word

--- hash.rb
+++ hash.rb
@@ -70,15 +70,21 @@
 
 # 8. Make an array of hashes to store the brand and model for 3 different laptops. Then print out the second laptop's model.
 
-laptops = [
-{"brand" => "Dell", "model" => "XPS"}, 
-{"brand" => "HP", "model" => "thinkpad"}, 
-{"brand" => "Apple", "model" => "air"}
+# laptops = [
+# {"brand" => "Dell", "model" => "XPS"}, 
+# {"brand" => "HP", "model" => "thinkpad"}, 
+# {"brand" => "Apple", "model" => "air"}
 
-]
+# ]
 
-p laptops[1]["model"]
+# p laptops[1]["model"]
 
 # 9. Make a hash to store definitions for 2 different words. Then add a new word and definition and print the hash to see the result.
+
+words = {"candid" => "honest", "platonic" => "friendly"}
+
+words["diatribe"] = "long rant"
+
+p words 
 
 # 10. Make a hash to store a shirt's brand, color, and size. Then print each attribute on separate lines.
