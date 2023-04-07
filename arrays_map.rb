f8ff0bac7e87eab4f079617d7e538edd138fb039
Miray Karabulut <mimikarabulut@gmail.com>
2023-04-07 13:49:03 -0500

    array upcase to new array

--- arrays_map.rb
+++ arrays_map.rb
@@ -1,18 +1,28 @@
 # 1. Start with an array of numbers and create a new array with each number times 3.
 #     For example, [1, 2, 3] becomes [3, 6, 9].
 
-numbers = [1, 2, 3]
-new_numbers = [ ]
-index = 0 
-while index < numbers.length 
-  new_numbers << numbers[index] * 3
-  index += 1 
-end 
-p new_numbers
+# numbers = [1, 2, 3]
+# new_numbers = [ ]
+# index = 0 
+# while index < numbers.length 
+#   new_numbers << numbers[index] * 3
+#   index += 1 
+# end 
+# p new_numbers
 
 
 #  2. Start with an array of strings and create a new array with each string upcased.
 #     For example, ["hello", "goodbye"] becomes ["HELLO", "GOODBYE"].
+
+string = ["hello", "goodbye"]
+new_string = []
+index = 0 
+while index < string.length 
+  new_string << string[index].upcase
+  index += 1
+end 
+
+p new_string
 
 #  3. Start with an array of hashes and create a new array of string values from each hash's :name key.
 #     For example, [{name: "Alice", age: 27}, {name: "Blane", age: 16}] becomes ["Alice", "Blane"].
