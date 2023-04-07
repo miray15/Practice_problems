53546379ee5bb93d0ef71911b22c915ddba18e4f
Miray Karabulut <mimikarabulut@gmail.com>
2023-04-07 13:55:59 -0500

    array grab first key from two hashes

--- arrays_map.rb
+++ arrays_map.rb
@@ -14,21 +14,38 @@
 #  2. Start with an array of strings and create a new array with each string upcased.
 #     For example, ["hello", "goodbye"] becomes ["HELLO", "GOODBYE"].
 
-string = ["hello", "goodbye"]
-new_string = []
-index = 0 
-while index < string.length 
-  new_string << string[index].upcase
-  index += 1
-end 
+# string = ["hello", "goodbye"]
+# new_string = []
+# index = 0 
+# while index < string.length 
+#   new_string << string[index].upcase
+#   index += 1
+# end 
 
-p new_string
+# p new_string
 
 #  3. Start with an array of hashes and create a new array of string values from each hash's :name key.
 #     For example, [{name: "Alice", age: 27}, {name: "Blane", age: 16}] becomes ["Alice", "Blane"].
 
+# hashes = [
+# {name: "Jennifer", last_name: "Lopez"}, 
+# {name: "Bennifer", last_name: "Afflack"}
+# ]
+
+# first_name = []
+
+# index = 0 
+# while index < hashes.length 
+#   first_name << hashes[index][:name]
+#   index += 1
+# end 
+
+# p first_name
+
 #  4. Start with an array of numbers and create a new array with each number plus 7.
 #     For example, [1, 2, 3] becomes [8, 9, 10].
+
+
 
 #  5. Start with an array of strings and create a new array with each string's length.
 #     For example, ["hello", "goodbye"] becomes [5, 7].
