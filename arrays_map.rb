aa9ec8998d620e2ace6a8890f093a2b13fc38f90
Miray Karabulut <mimikarabulut@gmail.com>
2023-04-09 15:52:16 -0500

    arrays capitalize strings

--- arrays_map.rb
+++ arrays_map.rb
@@ -1,16 +1,21 @@
 # 1. Start with an array of numbers and create a new array with each number times 3.
 #     For example, [1, 2, 3] becomes [3, 6, 9].
 
-numbers = [1, 2, 3]
-tripled = []
-tripled = numbers.map do |triple| 
-  triple * 3
-end 
+# numbers = [1, 2, 3]
+# tripled = []
+# tripled = numbers.map do |triple| 
+#   triple * 3
+# end 
 
-p tripled 
+# p tripled 
 
 #  2. Start with an array of strings and create a new array with each string upcased.
 #     For example, ["hello", "goodbye"] becomes ["HELLO", "GOODBYE"].
+
+strings = ["this", "octopus", "is", "smart"]
+caps = strings.map do |capitalize| capitalize.upcase
+end 
+  p caps 
 
 
 
