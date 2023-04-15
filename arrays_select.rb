45eaa03e3d2da5efb0e4d00ba4f5e47ae304d776
Miray Karabulut <mimikarabulut@gmail.com>
2023-04-15 18:05:01 -0500

    final commit for section

--- arrays_select.rb
+++ arrays_select.rb
@@ -114,12 +114,12 @@
 # 10. Start with an array of numbers and create a new array with only the odd numbers.
 #     For example, [2, 4, 5, 1, 8, 9, 7] becomes [5, 1, 9, 7].
 
-numbers = [2, 4, 5, 1, 8, 9, 7]
-even = []
-numbers.map do |odd|
-  if odd % 2 == 0 
-    even << odd 
-  else 
-    p odd 
-  end 
-end 
+# numbers = [2, 4, 5, 1, 8, 9, 7]
+# even = []
+# numbers.map do |odd|
+#   if odd % 2 == 0 
+#     even << odd 
+#   else 
+#     p odd 
+#   end 
+# end 
