b7e0eb281fa845b2e8f927481789cb5666f02ae7
Miray Karabulut <mimikarabulut@gmail.com>
2023-04-15 17:53:44 -0500

    hash of products if price < 10 p product map loop

--- arrays_select.rb
+++ arrays_select.rb
@@ -90,18 +90,26 @@
 #  8. Start with an array of strings and create a new array with only the strings that don't start with the letter "b".
 #     For example, ["big", "little", "good", "bad"] becomes ["little", "good"].
 
-strings =  ["big", "little", "good", "bad"]
-no = []
-strings.map do |dont_start|
-  if dont_start.chr == "b"
-    no << dont_start
-  else 
-    p dont_start
-  end 
-end 
+# strings =  ["big", "little", "good", "bad"]
+# no = []
+# strings.map do |dont_start|
+#   if dont_start.chr == "b"
+#     no << dont_start
+#   else 
+#     p dont_start
+#   end 
+# end 
 
 #  9. Start with an array of hashes and create a new array with only the hashes with prices less than 10 (from the :price key).
 #     For example, [{name: "chair", price: 100}, {name: "pencil", price: 1}, {name: "book", price: 4}] becomes [{name: "pencil", price: 1}, {name: "book", price: 4}].
+
+hashes = [{name: "chair", price: 100}, {name: "pencil", price: 1}, {name: "book", price: 4}]
+
+hashes.map do |not_above_ten|
+  if not_above_ten[:price] < 10
+    p not_above_ten
+  end 
+end 
 
 # 10. Start with an array of numbers and create a new array with only the odd numbers.
 #     For example, [2, 4, 5, 1, 8, 9, 7] becomes [5, 1, 9, 7].
