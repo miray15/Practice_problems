34ec63b8363515dfa89ad2c1f6440c04efb06596
Miray Karabulut <mimikarabulut@gmail.com>
2023-04-15 18:20:25 -0500

    p strings that start with w select method

--- arrays_select2.rb
+++ arrays_select2.rb
@@ -1,11 +1,15 @@
 #  1. Start with an array of numbers and create a new array with only the numbers less than 20.
 #     For example, [2, 32, 80, 18, 12, 3] becomes [2, 18, 12, 3].
 
-numbers = [2, 32, 80, 18, 12, 3]
-p numbers.select {|less_than_20| less_than_20 < 20 }
+# numbers = [2, 32, 80, 18, 12, 3]
+# p numbers.select {|less_than_20| less_than_20 < 20 }
 
 #  2. Start with an array of strings and create a new array with only the strings that start with the letter "w".
 #     For example, ["winner", "winner", "chicken", "dinner"] becomes ["winner", "winner"].
+
+words = ["winner", "winner", "chicken", "dinner"]
+p words.select {|w| w.chr == "w"}
+
 
 #  3. Start with an array of hashes and create a new array with only the hashes with prices greater than 5 (from the :price key).
 #     For example, [{name: "chair", price: 100}, {name: "pencil", price: 1}, {name: "book", price: 4}] becomes [{name: "chair", price: 100}].
