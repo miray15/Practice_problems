b311b26bb61a1e56f3e38d96e5aa356bf011eccd
Miray Karabulut <mimikarabulut@gmail.com>
2023-04-17 10:19:08 -0500

    return products with name less than 6 letters select method

--- arrays_select2.rb
+++ arrays_select2.rb
@@ -28,11 +28,15 @@
 #  5. Start with an array of strings and create a new array with only the strings shorter than 4 letters.
 #     For example, ["a", "man", "a", "plan", "a", "canal", "panama"] becomes ["a", "man", "a", "a"].
 
-words = ["a", "man", "a", "plan", "a", "canal", "panama"]
-p words.select {|four| four.length < 4}
+# words = ["a", "man", "a", "plan", "a", "canal", "panama"]
+# p words.select {|four| four.length < 4}
 
 #  6. Start with an array of hashes and create a new array with only the hashes with names shorter than 6 letters (from the :name key).
 #     For example, [{name: "chair", price: 100}, {name: "pencil", price: 1}, {name: "book", price: 4}] becomes [{name: "chair", price: 100}, {name: "book", price: 4}].
+
+
+products = [{name: "chair", price: 100}, {name: "pencil", price: 1}, {name: "book", price: 4}] 
+p products.select {|smaller| smaller[:name].to_i < 6}
 
 #  7. Start with an array of numbers and create a new array with only the numbers less than 10.
 #     For example, [8, 23, 0, 44, 1980, 3] becomes [8, 0, 3].
