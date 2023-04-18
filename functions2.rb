c818d627bcbc2f8ab11a4cd2b27ffb47f75177bd
Miray Karabulut <mimikarabulut@gmail.com>
2023-04-18 10:49:50 -0500

    function subtract two numbers

--- functions2.rb
+++ functions2.rb
@@ -8,13 +8,19 @@
 
 # 2. Write a function that takes in a string and returns the string with all capital letters. Then run the function and print the result.
 
-def caps(string)
-  return string.upcase
-end 
+# def caps(string)
+#   return string.upcase
+# end 
 
-p caps("hello")
+# p caps("hello")
 
 # 3. Write a function that takes in two numbers and returns the first number subtracted by the second. Then run the function and print the result.
+
+def add(number1, number2)
+  return number1 - number2
+end 
+
+p add(5, 1)
 
 # 4. Write a function that takes in a number and returns the number times itself. Then run the function and print the result.
 
