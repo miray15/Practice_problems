c6e847199f0a5397c3b10b0db291f0de45908462
Miray Karabulut <mimikarabulut@gmail.com>
2023-04-22 20:52:13 -0500

    function - return number times itself

--- functions3.rb
+++ functions3.rb
@@ -14,12 +14,17 @@
 
 # 3. Write a function that takes in two numbers and returns the first number subtracted by the second. Then run the function and print the result.
 
-def subtraction(number1, number2)
-  return number1 - number2
-end 
-p subtraction(5, 1)
+# def subtraction(number1, number2)
+#   return number1 - number2
+# end 
+# p subtraction(5, 1)
 
 # 4. Write a function that takes in a number and returns the number times itself. Then run the function and print the result.
+
+def times(number)
+  return number * number
+end 
+p times(5)
 
 # 5. Write a function that takes in a string and returns the first letter of the string. Then run the function and print the result.
 
