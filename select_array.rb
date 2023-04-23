79b5abf1eb9895b9668554b25bdc6a562c24e873
Miray Karabulut <mimikarabulut@gmail.com>
2023-04-22 21:23:38 -0500

    select - p words that start with w from a string

--- select_array.rb
+++ select_array.rb
@@ -1,16 +1,20 @@
 #  1. Start with an array of numbers and create a new array with only the numbers less than 20.
 #     For example, [2, 32, 80, 18, 12, 3] becomes [2, 18, 12, 3].
 
-numbers = [2, 32, 80, 18, 12, 3]
-twenty = numbers.select do |number|
-  number < 20 
-end 
-p twenty
+# numbers = [2, 32, 80, 18, 12, 3]
+# twenty = numbers.select do |number|
+#   number < 20 
+# end 
+# p twenty
 
 #  2. Start with an array of strings and create a new array with only the strings that start with the letter "w".
 #     For example, ["winner", "winner", "chicken", "dinner"] becomes ["winner", "winner"].
 
-
+strings = ["winner", "winner", "chicken", "dinner"]
+w = strings.select do |words| 
+words[0] == "w"
+end 
+p w
 
 
 #  3. Start with an array of hashes and create a new array with only the hashes with prices greater than 5 (from the :price key).
