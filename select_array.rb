b617d6378004141b87dd31115b18d95f4a5227f3
Miray Karabulut <mimikarabulut@gmail.com>
2023-04-22 21:25:42 -0500

    select - p prices greater than 100

--- select_array.rb
+++ select_array.rb
@@ -10,17 +10,22 @@
 #  2. Start with an array of strings and create a new array with only the strings that start with the letter "w".
 #     For example, ["winner", "winner", "chicken", "dinner"] becomes ["winner", "winner"].
 
-strings = ["winner", "winner", "chicken", "dinner"]
-w = strings.select do |words| 
-words[0] == "w"
-end 
-p w
+# strings = ["winner", "winner", "chicken", "dinner"]
+# w = strings.select do |words| 
+# words[0] == "w"
+# end 
+# p w
 
 
 #  3. Start with an array of hashes and create a new array with only the hashes with prices greater than 5 (from the :price key).
 #     For example, [{name: "chair", price: 100}, {name: "pencil", price: 1}, {name: "book", price: 4}] becomes [{name: "chair", price: 100}].
 
+hashes = [{name: "chair", price: 100}, {name: "pencil", price: 1}, {name: "book", price: 4}]
 
+prices = hashes.select do |price|
+  price[:price] > 5
+end 
+p prices 
 #  4. Start with an array of numbers and create a new array with only the even numbers.
 #     For example, [2, 4, 5, 1, 8, 9, 7] becomes [2, 4, 8].
 
