0c9c3433d55dfcaf35dfe0ae35db558b6a605fc0
Miray Karabulut <mimikarabulut@gmail.com>
2023-04-22 21:21:50 -0500

    select - numbers less than 20

--- functions3.rb
+++ functions3.rb
@@ -62,7 +62,7 @@
 
 # 10. Write a function that takes in a number and returns the number times 10 plus 30. Then run the function and print the result.
 
-def times_plus(number)
-  return number * 10 + 30
-end 
-p times_plus(10)
+# def times_plus(number)
+#   return number * 10 + 30
+# end 
+# p times_plus(10)
--- select_array.rb
+++ select_array.rb
@@ -1,93 +1,51 @@
 #  1. Start with an array of numbers and create a new array with only the numbers less than 20.
 #     For example, [2, 32, 80, 18, 12, 3] becomes [2, 18, 12, 3].
 
-# numbers = [2, 32, 80, 18, 12, 3]
-# less_than_20 = numbers.select do |less|
-# less < 20
-# end 
-# p less_than_20
+numbers = [2, 32, 80, 18, 12, 3]
+twenty = numbers.select do |number|
+  number < 20 
+end 
+p twenty
 
 #  2. Start with an array of strings and create a new array with only the strings that start with the letter "w".
 #     For example, ["winner", "winner", "chicken", "dinner"] becomes ["winner", "winner"].
 
-# words = ["winner", "winner", "chicken", "dinner"]
 
-# w_only = words.select do |w|
-#   w[0] == "w"
-# end 
-# p w_only
 
 
 #  3. Start with an array of hashes and create a new array with only the hashes with prices greater than 5 (from the :price key).
 #     For example, [{name: "chair", price: 100}, {name: "pencil", price: 1}, {name: "book", price: 4}] becomes [{name: "chair", price: 100}].
 
-# things = [{name: "chair", price: 100}, {name: "pencil", price: 1}, {name: "book", price: 4}] 
 
-# lower = things.select do |prices|
-#   prices[:price] < 5
-# end 
-# p lower 
 #  4. Start with an array of numbers and create a new array with only the even numbers.
 #     For example, [2, 4, 5, 1, 8, 9, 7] becomes [2, 4, 8].
 
-# numbers = [2, 4, 5, 1, 8, 9, 7]
-# two = numbers.select do |even|
-#   even.even?
-# end 
-# p two 
+ 
 
 #  5. Start with an array of strings and create a new array with only the strings shorter than 4 letters.
 #     For example, ["a", "man", "a", "plan", "a", "canal", "panama"] becomes ["a", "man", "a", "a"].
 
-# words = ["a", "man", "a", "plan", "a", "canal", "panama"]
-# shorter = words.select do |four|
-#   four.length < 4
-# end 
-# p shorter 
+
 
 #  6. Start with an array of hashes and create a new array with only the hashes with names shorter than 6 letters (from the :name key).
 #     For example, [{name: "chair", price: 100}, {name: "pencil", price: 1}, {name: "book", price: 4}] becomes [{name: "chair", price: 100}, {name: "book", price: 4}].
 
-# words = [{name: "chair", price: 100}, {name: "pencil", price: 1}, {name: "book", price: 4}]
-# shorter = words.select do |six|
-#   six[:name].length < 6
-# end 
-# p shorter 
+
 #  7. Start with an array of numbers and create a new array with only the numbers less than 10.
 #     For example, [8, 23, 0, 44, 1980, 3] becomes [8, 0, 3].
 
-# numbers = [8, 23, 0, 44, 1980, 3]
-# new_numbers = numbers.select do |less_than|
-#   less_than < 10
-# end 
-# p new_numbers 
+
 
 
 #  8. Start with an array of strings and create a new array with only the strings that don't start with the letter "b".
 #     For example, ["big", "little", "good", "bad"] becomes ["little", "good"].
 
 
-# words = ["big", "little", "good", "bad"]
-# not_b = words.select do |b|
-#   b.chr != "b"
-# end 
-# p not_b
 
 #  9. Start with an array of hashes and create a new array with only the hashes with prices less than 10 (from the :price key).
 #     For example, [{name: "chair", price: 100}, {name: "pencil", price: 1}, {name: "book", price: 4}] becomes [{name: "pencil", price: 1}, {name: "book", price: 4}].
 
-# things = [{name: "chair", price: 100}, {name: "pencil", price: 1}, {name: "book", price: 4}]
-
-# less = things.select do |prices|
-#   prices[:price] < 10 
-# end 
-# p less 
 
 # 10. Start with an array of numbers and create a new array with only the odd numbers.
 #     For example, [2, 4, 5, 1, 8, 9, 7] becomes [5, 1, 9, 7].
 
-# numbers = [2, 4, 5, 1, 8, 9, 7] 
-# odd = numbers.select do |number|
-#   number % 2 == 1 
-# end 
-# p odd 
