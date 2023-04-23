f8c905fbb2df7e87b280f71c901e548f5a8ecfda
Miray Karabulut <mimikarabulut@gmail.com>
2023-04-22 21:16:03 -0500

    function - number times 10 plus 30

--- functions3.rb
+++ functions3.rb
@@ -55,9 +55,14 @@
 
 # 9. Write a function that takes in 3 numbers and returns the average (the sum divided by 3.0). Then run the function and print the result.
 
-def average(number1, number2, number3)
-return (number1 + number2 + number3) / 3 
-end 
-p average(3, 3, 3)
+# def average(number1, number2, number3)
+# return (number1 + number2 + number3) / 3 
+# end 
+# p average(3, 3, 3)
 
 # 10. Write a function that takes in a number and returns the number times 10 plus 30. Then run the function and print the result.
+
+def times_plus(number)
+  return number * 10 + 30
+end 
+p times_plus(10)
