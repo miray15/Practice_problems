c9adbbc100fbf444f17477c425d0be1f44846969
Miray Karabulut <mimikarabulut@gmail.com>
2023-04-22 21:00:35 -0500

    function - return number as string

--- functions3.rb
+++ functions3.rb
@@ -35,13 +35,17 @@
 
 # 6. Write a function that takes in three strings and returns a string that combines all three strings with spaces in between. Then run the function and print the result.
 
-def string(string1, string2, string3)
-  return "#{string1} #{string2} #{string3}"
-end 
-p string("hello", "there", "whale")
+# def string(string1, string2, string3)
+#   return "#{string1} #{string2} #{string3}"
+# end 
+# p string("hello", "there", "whale")
 
 # 7. Write a function that takes in a number and returns the number as a string. Then run the function and print the result.
 
+def string(number)
+  return number.to_s
+end 
+p string(1)
 # 8. Write a function that takes in a string and returns the string repeated 5 times. Then run the function and print the result.
 
 # 9. Write a function that takes in 3 numbers and returns the average (the sum divided by 3.0). Then run the function and print the result.
