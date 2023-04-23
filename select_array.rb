a40a6e96e80da936cf9e24073a8ca8ddb1ee05b5
Miray Karabulut <mimikarabulut@gmail.com>
2023-04-22 19:19:05 -0500

    select print items cheaper than 5

--- select_array.rb
+++ select_array.rb
@@ -10,17 +10,23 @@
 #  2. Start with an array of strings and create a new array with only the strings that start with the letter "w".
 #     For example, ["winner", "winner", "chicken", "dinner"] becomes ["winner", "winner"].
 
-words = ["winner", "winner", "chicken", "dinner"]
+# words = ["winner", "winner", "chicken", "dinner"]
 
-w_only = words.select do |w|
-  w[0] == "w"
-end 
-p w_only
+# w_only = words.select do |w|
+#   w[0] == "w"
+# end 
+# p w_only
 
 
 #  3. Start with an array of hashes and create a new array with only the hashes with prices greater than 5 (from the :price key).
 #     For example, [{name: "chair", price: 100}, {name: "pencil", price: 1}, {name: "book", price: 4}] becomes [{name: "chair", price: 100}].
 
+things = [{name: "chair", price: 100}, {name: "pencil", price: 1}, {name: "book", price: 4}] 
+
+lower = things.select do |prices|
+  prices[:price] < 5
+end 
+p lower 
 #  4. Start with an array of numbers and create a new array with only the even numbers.
 #     For example, [2, 4, 5, 1, 8, 9, 7] becomes [2, 4, 8].
 
